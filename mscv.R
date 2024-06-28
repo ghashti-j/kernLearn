0889dc4a91c237b4e2089cfc7d13da274441eeea
ghashti-j <168594337+ghashti-j@users.noreply.github.com>
2024-06-28 13:24:16 -0700

    Update and rename mscv.R to mscv_kdsum.R

--- mscv.R
+++
@@ -1,7 +0,0 @@
-#######################################################
-# The MSCV objective function for optimal bandwidth calculation as introduced in
-# Ghashti, J. S., & Thompson, J. R. (2023). Kernel Metric Learning for Clustering Mixed-type Data. arXiv preprint arXiv:2306.01890.
-
-# To be used prior to using the kdsum function in the kdsum.R file as the bw argument vector
-# Note that any bandwidth selection technique may be used, but will not have the features outline in above paper
-#######################################################
